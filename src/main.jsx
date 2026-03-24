3b8968b00a6767f7b8d121df8809a2b91fe7b735
Andrew Crouch <crouch36@gmail.com>
2026-03-23 22:50:58 -0400

    fix: remove StrictMode to stop double fetchTrips causing duplicate trip cards

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
@@ -6,12 +5,10 @@ import App from './App.jsx'
 import TripPage from './TripPage.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/trips/:id" element={<TripPage />} />
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/trips/:id" element={<TripPage />} />
+      <Route path="/*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 )
